ff838098dd157455b62721faefff3d1ff268c95f
Hwasil <127382144+Hwasil@users.noreply.github.com>
2024-02-03 19:47:17 +0900

    Update ex8_6.c

--- DataStructure/Ch8_ex6/ex8_6.c
+++ DataStructure/Ch8_ex6/ex8_6.c
@@ -9,24 +9,24 @@ int main(void) {
 	// 그래프 G9 구성 : 정점 u에 대한 간선 (u,v)의 삽입순서는 v가 큰 것부터.
 	for (i = 0; i < 8; i++)
 		insertVertex(G9, i);
-	insertEdge(G9, 0, 5);
-	insertEdge(G9, 0, 1);
-	insertEdge(G9, 1, 3);
-	insertEdge(G9, 1, 2);
-	insertEdge(G9, 1, 0);
-	insertEdge(G9, 2, 1);
-	insertEdge(G9, 3, 6);
-	insertEdge(G9, 3, 5);
-	insertEdge(G9, 3, 4);
-	insertEdge(G9, 3, 1);
-	insertEdge(G9, 4, 7);
-	insertEdge(G9, 4, 6);
-	insertEdge(G9, 4, 3);
-	insertEdge(G9, 5, 3);
-	insertEdge(G9, 5, 0);
-	insertEdge(G9, 6, 4);
-	insertEdge(G9, 6, 3);
-	insertEdge(G9, 7, 4);
+		insertEdge(G9, 0, 5);
+		insertEdge(G9, 0, 1);
+		insertEdge(G9, 1, 3);
+		insertEdge(G9, 1, 2);
+		insertEdge(G9, 1, 0);
+		insertEdge(G9, 2, 1);
+		insertEdge(G9, 3, 6);
+		insertEdge(G9, 3, 5);
+		insertEdge(G9, 3, 4);
+		insertEdge(G9, 3, 1);
+		insertEdge(G9, 4, 7);
+		insertEdge(G9, 4, 6);
+		insertEdge(G9, 4, 3);
+		insertEdge(G9, 5, 3);
+		insertEdge(G9, 5, 0);
+		insertEdge(G9, 6, 4);
+		insertEdge(G9, 6, 3);
+		insertEdge(G9, 7, 4);
 	printf("\n Ch8_6 의 인접 리스트 ");
 	print_adjList(G9);  //G9의 인접 리스트를 확인용으로 출력
 
